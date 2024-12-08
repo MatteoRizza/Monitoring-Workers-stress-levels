ba4ab4b2df459490471b8e6ddc933bc77e7760cd
Matteo Rizza <rizzamatteo1996@gmail.com>
2024-12-08 20:22:34 +0100

    uploaded code from prevoius projects

---
+++ app/src/main/java/polar/com/androidblesdk/Manager.java
@@ -0,0 +1,61 @@
+package polar.com.androidblesdk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+
+public class Manager extends AppCompatActivity {
+
+
+    AWSIoT awsIoT;
+    String[] subscriber = {"Leschiera", "Test"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.manager);
+
+
+        String user = this.getIntent().getStringExtra("user");
+        TextView txtUser = this.findViewById(R.id.txtWelcomeUser);
+        txtUser.setText(user);
+
+        awsIoT = new AWSIoT(this );
+
+        Button btnConn = this.findViewById(R.id.btnConnection);
+        btnConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(AWSIotMqttClientStatusCallback.AWSIotMqttClientStatus.Connected != awsIoT.statusConnection){
+                            try {
+                                wait(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        for(int  i = 0; i< subscriber.length; i++) {
+                            awsIoT.subscribe(subscriber[i]);
+                        }
+                    }
+                }).start();
+                view.setClickable(false);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        awsIoT.disconnect();
+    }
+
+}
